d5b0d45bc9f02b99f30939f09cfd302e1956fb1a
Christopher <cheathmcdougal@gmail.com>
2021-11-17 15:53:22 -0800

    Add project files.

---
+++ ConnectDots/Program.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace ConnectDots
+{
+    class Program
+    {
+        static Vector3[] points = {
+            new Vector3(0,0,0),
+            new Vector3(3,0,0),
+            new Vector3(0,0,0),
+            new Vector3(-3,0,0)
+        };
+
+        static int pointsIndex;
+
+        static Vector3 _vect = new Vector3(0, 0, 0);
+
+        static bool isRunning;
+
+        static void Main(string[] args)
+        {
+            pointsIndex = 0;
+
+            isRunning = true;
+            Console.S
+            while(isRunning)
+            {
+                System.Threading.Thread.Sleep(100);
+                IncrementRotation();
+                Console.WriteLine($"Vector3 - x: {_vect.x} y: {_vect.y} z: {_vect.z}");
+
+            }
+
+            Console.ReadKey();
+        }
+
+        #region FunMethods
+
+        static void IncrementRotation()
+        {
+            //Console.WriteLine($"\npointsIndex: {pointsIndex}\npoints.Length: {points.Length}");
+
+            if (pointsIndex == points.Length)
+            {
+                pointsIndex = 0;
+                return;
+            }
+
+            if (Math.Round(_vect.x) == points[pointsIndex].x)
+            {
+                pointsIndex++;
+                return;
+            }
+
+            if (_vect.x != points[pointsIndex].x)
+            {
+                _vect.x += Crawl(_vect.x, points[pointsIndex].x); // * Time.deltaTime; 
+            }
+        }
+
+        static float Crawl(float val, float direction)
+        {
+
+            // move towards the desired value (direction)
+
+            float returnValue = 0.0f;
+     
+            if (val == direction)
+            { 
+                return returnValue; // the value is the same
+            }
+
+            var temp = val;
+
+            temp += 0.1f;
+
+            if (GetDifference(direction,temp) < GetDifference(direction,val))
+            {
+                return 0.1f;
+            }
+
+            temp = val; // reset
+            temp += -0.1f;
+
+            if (GetDifference(direction,temp) < GetDifference(direction,val))
+            {
+                return -0.1f;
+            }
+
+            return returnValue;
+
+        }
+
+        static float GetDifference(float a, float b)
+        {
+            float lower, higher; 
+
+            if(a > b) { lower = b; higher = a; } else { lower = a; higher = b; }
+
+            var f = (higher - lower);
+
+            return f;
+        }
+
+        #endregion
+    }
+}
---
+++ ConnectDots/Vector3.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConnectDots
+{
+    class Vector3
+    {
+        public float x, y, z;
+        public Vector3(float x, float y, float z)
+        {
+            this.x = x;
+            this.z = z;
+            this.y = y;
+        }
+    }
+}
